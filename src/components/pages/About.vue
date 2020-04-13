dbf65d30598ef7c323a8c67742a6a6b2024824c3
Bobby <bobbysebolao@gmail.com>
2020-04-14 00:20:25 +0100

    Update about page

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="content">
     <h2 class="pageHeader">About</h2>
-    <p>I (<a href="https://nienke.dev">Nienke</a>) like to log what I read and watch in a year. Find the source code on GitHub:</p>
+    <p>I (<a href="https://www.bobbysebolao.com/">Bobby</a>) like to log what I read and watch in a year. Find the source code on GitHub:</p>
     <ul>
-      <li><a href="https://github.com/nienkedekker/whatpm-client">Client</a></li>
-      <li><a href="https://github.com/nienkedekker/whatpm-server">Server</a></li>
+      <li><a href="https://github.com/bobbysebolao/thepopculturetrip-client">Client</a></li>
+      <li><a href="https://github.com/bobbysebolao/thepopculturetrip-server">Server</a></li>
     </ul>
   </section>
 </template>
